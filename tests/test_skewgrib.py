ccf0ccab7d337a3f66759b8dbca009f9b00119d4
chrisobrien <chris.obrien.1021@gmail.com>
2020-12-13 12:16:42 -0800

    Refactor directory structure

--- tests/test_skewgrib.py
+++ tests/test_skewgrib.py
@@ -1,6 +1,6 @@
-from skewgrib import skewgrib
+from skewgrib.grib2 import grib2
 from metpy.units import units
 
 class TestClass:
     def test_return_temp(self):
-        assert skewgrib.return_temp() == units.Quantity(17.552667236328148, 'degree_Celsius')
+        assert grib2.return_temp() == units.Quantity(17.552667236328148, 'degree_Celsius')
